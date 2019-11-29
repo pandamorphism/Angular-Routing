b20b9ed17538bae626f441d7e4887b289bd02c02
throwable <uakennysoft@gmail.com>
2019-11-29 18:36:42 +0200

    initial

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import {ProductListComponent} from './product-list.component';
+import {ProductDetailComponent} from './product-detail.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
 
-import { SharedModule } from '../shared/shared.module';
+import {SharedModule} from '../shared/shared.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'products',
+        component: ProductListComponent
+      }
+    ])
   ],
   declarations: [
     ProductListComponent,
@@ -16,4 +23,5 @@ import { SharedModule } from '../shared/shared.module';
     ProductEditComponent
   ]
 })
-export class ProductModule { }
+export class ProductModule {
+}
--- APM-Start/src/app/user/user.module.ts
+++ APM-Start/src/app/user/user.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
 
-import { SharedModule } from '../shared/shared.module';
+import {SharedModule} from '../shared/shared.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
   ],
   declarations: [
     LoginComponent
   ]
 })
-export class UserModule { }
+export class UserModule {
+}
